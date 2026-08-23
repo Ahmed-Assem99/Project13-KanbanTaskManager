c362d13aed0904d62b3ef5dae617386f79f722e8
Ahmed El Fakharany <ahmedassem13@gmail.com>
2026-08-23 13:43:13 +0300

    tasks saving in local storage feature

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@
     const addTaskBtn = document.querySelector("#add-task-btn");
     const modalOverlay = document.querySelector("#modal-overlay");
     const closeModalBtn = document.querySelector("#close-modal-btn");
+    const modalTitle = document.querySelector("#modal-title");
+    const taskTitle = document.querySelector("#task-title");
+    const titleError = document.querySelector("#title-error");
+    const taskPriority = document.querySelector("#task-priority");
+    const taskDueDate = document.querySelector("#task-due-date");
+    const dateError = document.querySelector("#date-error");
+    const taskDescription = document.querySelector("#task-description");
+    const descriptionError = document.querySelector("#description-error");
     const cancelBtn = document.querySelector("#cancel-btn");
+    const submitBtn = document.querySelector("#submit-btn");
+    let tasks;
+    tasks = JSON.parse(localStorage.getItem('TaskHistory') ?? '[]');
     function closeModal() {
         modalOverlay?.classList.add("hidden");
         modalOverlay?.classList.remove("flex");
@@ -26,4 +37,53 @@
     cancelBtn?.addEventListener("click", e => {
         closeModal();
     });
+    submitBtn?.addEventListener("click", e => {
+        e.preventDefault();
+        if (!taskTitle?.value) {
+            taskTitle?.classList.replace('focus:ring-indigo-500', 'focus:ring-red-500');
+            taskTitle?.classList.replace('focus:border-indigo-500', 'focus:border-red-500');
+            taskTitle?.classList.replace('border-slate-300', 'border-red-500');
+            titleError?.classList.remove('hidden');
+            return;
+        }
+        else if (taskDueDate?.value && new Date(taskDueDate.value) < new Date()) {
+            taskDueDate?.classList.replace('focus:ring-indigo-500', 'focus:ring-red-500');
+            taskDueDate?.classList.replace('focus:border-indigo-500', 'focus:border-red-500');
+            taskDueDate?.classList.replace('border-slate-300', 'border-red-500');
+            dateError?.classList.remove('hidden');
+            return;
+        }
+        else {
+            taskTitle?.classList.replace('focus:ring-red-500', 'focus:ring-indigo-500');
+            taskTitle?.classList.replace('focus:border-red-500', 'focus:border-indigo-500');
+            taskTitle?.classList.replace('border-red-500', 'border-slate-300');
+            taskDueDate?.classList.replace('focus:ring-red-500', 'focus:ring-indigo-500');
+            taskDueDate?.classList.replace('focus:border-red-500', 'focus:border-indigo-500');
+            taskDueDate?.classList.replace('border-red-500', 'border-slate-300');
+            titleError?.classList.add('hidden');
+            dateError?.classList.add('hidden');
+            addNewTask();
+            closeModal();
+        }
+    });
+    function addNewTask() {
+        let newTask = {
+            title: taskTitle.value,
+            priority: taskPriority?.value,
+            date: taskDueDate?.value,
+            description: taskDescription?.value
+        };
+        tasks.push(newTask);
+        console.log(tasks);
+        localStorage.setItem('TaskHistory', JSON.stringify(tasks));
+        clearForum();
+    }
+    function clearForum() {
+        taskTitle.value = '';
+        taskPriority.value = '';
+        taskDueDate.value = '';
+        taskDescription.value = '';
+    }
+    function displayNewTask() {
+    }
 })();
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,27 @@
     const addTaskBtn:HTMLButtonElement|null = document.querySelector("#add-task-btn")
     const modalOverlay:Element|null = document.querySelector("#modal-overlay")
     const closeModalBtn:HTMLButtonElement|null = document.querySelector("#close-modal-btn")
+    const modalTitle:Element|null = document.querySelector("#modal-title")
+    const taskTitle:HTMLInputElement|null= document.querySelector("#task-title")
+    const titleError:Element|null = document.querySelector("#title-error")
+    const taskPriority:HTMLInputElement|null=document.querySelector("#task-priority")
+    const taskDueDate:HTMLInputElement|null=document.querySelector("#task-due-date")
+    const dateError:Element|null = document.querySelector("#date-error")
+    const taskDescription:HTMLInputElement|null=document.querySelector("#task-description")
+    const descriptionError:Element|null=document.querySelector("#description-error")
     const cancelBtn:HTMLButtonElement|null = document.querySelector("#cancel-btn")
+    const submitBtn:HTMLButtonElement|null = document.querySelector("#submit-btn")
 
+    interface Task{
+    title:string,
+    priority?:string,
+    date?:string,
+    description?:string
+}
 
+    let tasks:Array<Task>;
+    tasks=JSON.parse(localStorage.getItem('TaskHistory')??'[]')
+ 
 
 function closeModal():void{
 modalOverlay?.classList.add("hidden")
@@ -32,4 +50,64 @@ closeModal()
 closeModal()
     })
 
+submitBtn?.addEventListener("click",e=>{
+    e.preventDefault(); 
+    if(!taskTitle?.value){
+taskTitle?.classList.replace('focus:ring-indigo-500','focus:ring-red-500')
+taskTitle?.classList.replace('focus:border-indigo-500','focus:border-red-500')
+taskTitle?.classList.replace('border-slate-300','border-red-500')
+titleError?.classList.remove('hidden')
+return
+    }else if(taskDueDate?.value && new Date(taskDueDate.value) < new Date()){
+taskDueDate?.classList.replace('focus:ring-indigo-500','focus:ring-red-500')
+taskDueDate?.classList.replace('focus:border-indigo-500','focus:border-red-500')
+taskDueDate?.classList.replace('border-slate-300','border-red-500')
+dateError?.classList.remove('hidden')
+return
+    }
+    else{
+    taskTitle?.classList.replace('focus:ring-red-500','focus:ring-indigo-500')
+taskTitle?.classList.replace('focus:border-red-500','focus:border-indigo-500')
+taskTitle?.classList.replace('border-red-500','border-slate-300') 
+
+taskDueDate?.classList.replace('focus:ring-red-500','focus:ring-indigo-500')
+taskDueDate?.classList.replace('focus:border-red-500','focus:border-indigo-500')
+taskDueDate?.classList.replace('border-red-500','border-slate-300')
+
+    titleError?.classList.add('hidden')
+    dateError?.classList.add('hidden')  
+    addNewTask();
+    closeModal();
+    }
+})
+
+function addNewTask():void{
+
+    let newTask:Task={
+title:taskTitle!.value,
+priority:taskPriority?.value,
+date:taskDueDate?.value,
+description:taskDescription?.value
+    }
+tasks.push(newTask)
+console.log(tasks)
+localStorage.setItem('TaskHistory',JSON.stringify(tasks))
+clearForum();
+}
+
+function clearForum():void{
+    taskTitle!.value='';
+    taskPriority!.value='';
+    taskDueDate!.value='';
+    taskDescription!.value='';
+}
+
+function displayNewTask():void{
+    
+}
+
+
 })()
+
+
+
